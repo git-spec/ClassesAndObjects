4309b70bcd5304ef0a79f1d2eeaaf523cfbeed4f
git-spec <inkyfischer@gmail.com>
2025-04-01 14:21:27 +0200

    feat: Created class Vehicle.

---
+++ src/main/java/org/example/Vehicle.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Vehicle {
+    private final String manufacturer;
+    private final  String model;
+    private final int year;
+    
+    public Vehicle(String manufacturer, String model, int year) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.year = year;
+    }
+}
