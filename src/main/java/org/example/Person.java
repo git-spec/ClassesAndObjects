f7c300de456088df5f1095521ab0ca7ef9c9c4c8
git-spec <inkyfischer@gmail.com>
2025-04-01 12:59:28 +0200

    refactor: Added getters and setters to class Person.

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -11,6 +11,30 @@ public class Person {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void introduce() {
         System.out.println("Hello, my name is " + name + ".");
         System.out.println("I am " + gender + " and " + age + " years old.\n");
