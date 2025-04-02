8dd6ef582a308bf9dcac33779e6a3e887fee4fba
git-spec <inkyfischer@gmail.com>
2025-04-02 11:12:37 +0200

    style: Formatted outputs and those of vehicles adjusted.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,28 +4,31 @@ public class Main {
     public static void main(String[] args) {
         // Vehicle
         Vehicle Plane = new Vehicle("Airbus", "plane", 2024);
-        System.out.println(Plane);
+        System.out.println(Plane + "\n");
         // Golf 1
         Car Golf_1 = new Car("VW", "car", "VW", "Golf", "black", 2025);
         Golf_1.startCar();
         System.out.println(Golf_1);
         // Golf 2
         Car Golf_2 = new Car("VW", "car","VW", "Golf", "white", 2023);
-        System.out.println(Golf_2.getSpeed());
+        System.out.println("actual speed of Golf: " + Golf_2.getSpeed());
         Golf_2.setSpeed(60);
-        System.out.println(Golf_2.getSpeed());
-        Motorcycle Bike = new Motorcycle("FIAT", "bike", 2006, "Ducati", "GT 1000 Classic", "silver");
-        System.out.println(Bike);
+        System.out.println("accelerated speed of Golf: " + Golf_2.getSpeed() + "\n");
+        Motorcycle Bike_1 = new Motorcycle("FIAT", "bike", 2006, "Ducati", "GT 1000 Classic", "silver");
+        Motorcycle Bike_2 = new Motorcycle("Triumph", "bike", 1972, "Triumph", "Bonneville T120", "gold");
+        System.out.println(Bike_1);
+        System.out.println(Bike_2 + "\n");
         // POLYMORPHISM
         Vehicle[] vehicles = {Plane, Golf_1, Golf_2};
         // Static numbers
+        Bike_2.setInstanceNumber(2);
+        Bike_1.setInstanceNumber(1);
         Car.setNumber(6);
-        System.out.println("Numbers of cars: " + Car.number);
-        Motorcycle.setNumber(2);
-        Bike.setInstanceNumber(1);
+        System.out.println("Numbers of Ducati: " + Bike_1.getInstanceNumber());
+        System.out.println("Numbers of Triumph: " + Bike_2.getInstanceNumber());
         System.out.println("Numbers of motorcycles: " + Motorcycle.getNumber());
-        System.out.println("Numbers of Ducati: " + Bike.getInstanceNumber());
-        System.out.println("Numbers of vehicles: " + Vehicle.number);
+        System.out.println("Numbers of cars: " + Car.number);
+        System.out.println("Numbers of vehicles: " + Vehicle.number + "\n");
         // Persons
         Person Jack = new Person("Jack", "male", 26);
         Jack.introduce();
